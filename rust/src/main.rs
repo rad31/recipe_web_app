24ae84c716ba885617d4ec5063840b7ad9da4b35
Ronny Dorling <32966672+rad31@users.noreply.github.com>
2023-09-23 11:26:37 -0700

    Add service for getting food by description

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,25 +1,16 @@
 pub mod models;
+pub mod services;
 
-use crate::models::food_name::FoodName;
+use crate::services::db_service;
 
 fn main() {
     let path = "../nutrient_db/nutrients.db";
     let connection = rusqlite::Connection::open(path).unwrap();
-    let query = "SELECT * FROM food_name WHERE food_description LIKE 'beef, ground%'";
-    let mut statement = connection.prepare(query).unwrap();
-    let rows = statement.query_map([], |row| {
-        FoodName::try_from(row)
-    });
-    
-    match rows {
-        Ok(rows) => {
-            for r in rows {
-                match r {
-                    Ok(food) => println!("{:?}", food.name),
-                    Err(e) => println!("{:?}", e)
-                }
-            }
-        },
-        Err(e) => println!("{:?}", e),
+
+    let foods = db_service::get_foods_by_desc("onion", &connection);
+
+    match foods {
+        Ok(foods) => for f in foods { println!("{:?}", f.name) },
+        Err(e) => println!("{:?}", e)
     }
 }
