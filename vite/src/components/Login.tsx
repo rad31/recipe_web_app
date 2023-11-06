5e3840b7992879aef2a55ef145aef50dd11a9d7f
Ronny Dorling <32966672+rad31@users.noreply.github.com>
2023-11-05 22:58:56 -0800

    Update css

--- vite/src/components/Login.tsx
+++ vite/src/components/Login.tsx
@@ -76,46 +76,40 @@ function Login() {
             className="p-4 rounded-xl bg-green-secondary"
         >
             <div className="flex-col min-h-96 w-96">
-                <div>
-                    <form method="dialog">
-                        <div className="flex-col justify-items-center">
-                            {formFields.map((key) => (
-                                <div key={key} className="content-center pb-4">
-                                    <input
-                                        className="w-full rounded px-2 py-1"
-                                        placeholder={form[key]?.label}
-                                        type={form[key]?.type}
-                                        value={form[key]?.value}
-                                        onChange={(e) =>
-                                            setForm((prev) => ({
-                                                ...prev,
-                                                [key]: {
-                                                    ...form[key],
-                                                    value: e.target.value,
-                                                },
-                                            }))
-                                        }
-                                    />
-                                </div>
-                            ))}
-
-                            <div className="pb-4">
-                                <button
-                                    type="button"
-                                    onClick={() => submit()}
-                                    className="h-12 w-full rounded-lg bg-yellow-primary hover:bg-orange-primary"
-                                >
-                                    <span className="inline-block align-middle text-green-primary">
-                                        {isSignUp
-                                            ? "Create account"
-                                            : "Sign in"}
-                                    </span>
-                                </button>
-                            </div>
+                <form method="dialog" className="flex-col justify-center">
+                    {formFields.map((key) => (
+                        <div key={key} className="pb-4">
+                            <input
+                                className="w-full rounded px-2 py-1"
+                                placeholder={form[key]?.label}
+                                type={form[key]?.type}
+                                value={form[key]?.value}
+                                onChange={(e) =>
+                                    setForm((prev) => ({
+                                        ...prev,
+                                        [key]: {
+                                            ...form[key],
+                                            value: e.target.value,
+                                        },
+                                    }))
+                                }
+                            />
                         </div>
-                    </form>
-                </div>
-                <div>
+                    ))}
+
+                    <div className="pb-4">
+                        <button
+                            type="button"
+                            onClick={() => submit()}
+                            className="h-12 w-full rounded-lg bg-yellow-primary hover:bg-orange-primary"
+                        >
+                            <span className="inline-block align-middle text-green-primary">
+                                {isSignUp ? "Create account" : "Sign in"}
+                            </span>
+                        </button>
+                    </div>
+                </form>
+                <div className="flex justify-center">
                     <button onClick={changeForm} className="">
                         <span className="inline-block align-middle text-white hover:text-yellow-primary">
                             {isSignUp
